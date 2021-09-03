fe9fb784d76b3fbdf76065900be31629f893b426
Adrian <leongadrian36@gmail.com>
2021-09-03 18:02:45 +0800

    added counter button

---
+++ src/components/CounterButton.js
@@ -0,0 +1,29 @@
+import React, { PureComponent } from "react";
+
+class CounterButton extends PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = {
+			count: 0,
+		};
+	}
+	// shouldComponentUpdate(nextProps, nextState) {
+	// 	if (this.state.count !== nextState.count) return true;
+	// 	return false;
+	// }
+	updateCount = () => {
+		this.setState((state) => {
+			return { count: this.state.count + 1 };
+		});
+	};
+	render() {
+		console.log("CounterButton");
+		return (
+			<button color={this.props.color} onClick={this.updateCount}>
+				Count: {this.state.count}
+			</button>
+		);
+	}
+}
+
+export default CounterButton;
